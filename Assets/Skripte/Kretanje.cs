3ff8a63513958abad6196f4309df749ae2bafc05
Manuela174 <45518560+Manuela174@users.noreply.github.com>
2020-04-21 14:07:39 +0200

    ..
    ..

---
+++ Assets/Skripte/Kretanje.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Kretanje : MonoBehaviour
+{
+    public Text countText;
+    public float speed;
+    private int zbroj;
+    private Rigidbody rb;
+    private int count;
+
+    void Start()
+    {
+        //dohvaćanje komponente 
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+    }
+
+    void FixedUpdate ()
+    {
+        float kretanjeHorizontal = Input.GetAxis("Horizontal");
+        float kretanjeVertical = Input.GetAxis ("Vertical");
+
+        Vector3 kretanje = new Vector3 (kretanjeHorizontal,0.0f,
+            kretanjeVertical);
+
+       
+        rb.AddForce(speed*movement);
+    }   
+
+         void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+
+            zbroj += 1;
+
+            SetCountText ();
+        }
+    }
+    //postavlja tekst objekta Text 
+
+    void SetCountText ()
+    {
+
+        countText.text = "Rezultat: " + zbroj.ToString ();
+    }
+}
